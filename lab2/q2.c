a90f0a315615c0a7b6622806da4dad490ec307c1
Gaurav Pradeep <gauravpradeep2004@gmail.com>
2024-01-21 20:10:50 +0530

    fixed lab2 q2, i am very dumb

--- lab2/q2.c
+++ lab2/q2.c
@@ -1,86 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int count1=0;
-int count2=0;
-int opcount=0;
-void gcd(int m,int n){
-	// printf("int gcd function");
-	int fac1[100];
-	int fac2[100];
-	while(m%2==0){
-		m=m/2;
-		fac1[count1]=2;
-		count1=count1+1;
-	}
+int* primfacts(int n){
 
-	for(int i=3;i<sqrt(m);i=i+2)
-	{
-		printf("%d",m);
-		while(m%i==0)
-		{
-			fac1[count1]=i;
-			count1=count1+1;
-			m=m/i;
-		}
-	}
-	if(m>2)
-		fac1[count1++]=m;
-
-
+	int* factors=(int*)malloc(100*sizeof(int));
+	int t=n;
+	int count=0;
 
 	while(n%2==0){
 		n=n/2;
-		fac2[count2]=2;
-		count2=count2+1;
+		count++;
+		factors[0]=count;
+		factors[count]=2;
 	}
-
-	for(int i=3;i<sqrt(n);i=i+2)
+	for(int i=3;i<n;i=i+2)
 	{
 		while(n%i==0)
 		{
-			fac2[count2]=i;
-			count2=count2+1;
+			count++;
+			factors[count]=i;
 			n=n/i;
 		}
 	}
-	if(n>2)
-		fac2[count2++]=n;
-
-	
-	for(int i=0;i<count1;i++)
-	{
-		printf("%d ",fac1[i]);
+	if(n>2){
+		count++;
+		factors[0]=count;
+		factors[count]=n;
 	}
-	printf("\n");
-	// for(int i=0;i<count2;i++)
-	// {
-	// 	printf("%d ",fac2[i]);
-	// }
-
-	int i=0;
-	int j=0;
-	// int min=count1<count2?count1:count2;
-	int p=1;
-	for(int i=0;i<count1;i++)
-	{
-		for(j=0;j<count2;j++)
-		{
-			if(fac1[i]==fac2[j] && fac1[i]!=-1)
-			{
-				p=p*fac1[i];
-				fac1[i]=-1;
+	return factors;
+}
+void gcd(int* a, int* b){
+	int gcd=1;
+	for(int i=1;i<=a[a[0]];i++){
+		for(int j=1;j<=b[b[0]];j++){
+			if(a[i]==b[j] && b[j]!=-1){
+				gcd*=a[i];
+				a[i]=-1;
+				b[j]=-1;
+				break;
 			}
-
 		}
 	}
-	printf("opcount: %d",opcount);
-	printf("Gcd: %d",p);
+	printf("%d \n",gcd);
 }
 
 int main(){
-	int m,n;
-	printf("Enter 2 nos");
-	scanf("%d%d",&m,&n);
-	gcd(m,n);
+	int* a=primfacts(39);
+	int* b=primfacts(26);
+	gcd(a,b);
+	// for(int i=1;i<=a[a[0]];i++){
+	// 	printf("%d ",a[i]);
+	// }
+	// printf("\n");
 }
